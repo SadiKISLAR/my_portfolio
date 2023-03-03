cabff3bfce9f4125cdd9c88095cb9c609e96fc33
SadiKISLAR <sadi22kislar@gmail.com>
2023-03-03 15:06:27 +0300

    components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import About from './components/About';
+import Contacts from './components/Contacts';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
 
 function App() {
   return (
-    <></>
+    <div>
+      <Navbar />
+      <About />
+      <Skills />
+      <Projects />
+      <Contacts />
+      <Footer />
+    </div>
   );
 }
 
