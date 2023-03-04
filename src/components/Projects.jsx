669684f5059d8ebc9777fb523a8cff02d398ee58
SadiKISLAR <sadi22kislar@gmail.com>
2023-03-04 11:55:22 +0300

    projects start

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Projects = () => {
     return (
-        <div className='projects'>Projects</div>
+        <div className='projects'>
+            <div className="project p1"></div>
+            <div className="project p2"></div>
+            <div className="project p3"></div>
+            <div className="project p4"></div>
+            <div className="project p5"></div>
+            <div className="project p6"></div>
+            <div className="project p7"></div>
+            <div className="project p8"></div>
+            <div className="project p9"></div>
+            <div className="project p10"></div>
+            <div className="project p11"></div>
+            <div className="project p12"></div>
+            <div className="project p13"></div>
+            <div className="project p14"></div>
+            <div className="project p15"></div>
+
+        </div>
     )
 }
 
