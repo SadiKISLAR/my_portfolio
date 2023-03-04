dcdedf3ac858cc3661dc33693f0c583b33592106
SadiKISLAR <sadi22kislar@gmail.com>
2023-03-04 11:37:00 +0300

    skills added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MyPhoto from '../assets/images/picture.jpg'
+import MyPhoto from '../assets/images/bg.png'
 
 const About = () => {
     return (
@@ -9,7 +9,9 @@ const About = () => {
             </div>
             <div className="text">
                 <h1>Şadi KIŞLAR</h1>
+                <br />
                 <h2>Fullstack Developer</h2>
+                <br />
                 <p>Having 4+ years experienced in IT and 1+ an expert front-end developer, interested in writing codes, skilled at developing complex solutions, creating responsive designs, possessing strong creative thinking skills, high energy and integrity.
                     Ability to create algorithms effectively, and interact positively and communicate appropriately with team members. Quickly grasp new technologies and concepts to develop innovative and creative solutions to problems.
                     Always eager to learn various technologies, tools and libraries. Especially interested in the Frontend / Web Development, HTML, CSS, JS, React. Excited to learn new things and improve, lifetime student.</p></div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,136 @@
-import React from 'react'
+import React from "react";
 
 const Skills = () => {
     return (
-        <div className='skills'>Skills</div>
-    )
-}
+        <div className="skills">
+            <div className="skillsText">
+                <h1>SKILLS</h1>
 
-export default Skills
+                <p>Skills and programs that I actively use on my projects</p>
+            </div>
+            <div class="skillsLogo">
+                <div class="col-lg-2 col-md-4 col-sm-6  animation__item1">
+                    <div class="technology__box">
+                        <img
+                            src="https://www.vectorlogo.zone/logos/w3_html5/w3_html5-ar21.svg"
+                            class="img-fluid"
+                            alt="html"
+                        />
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-sm-6 animation__item2">
+                    <div class="technology__box">
+                        <img
+                            src="https://www.vectorlogo.zone/logos/w3_css/w3_css-ar21.svg"
+                            class="img-fluid"
+                            alt="css"
+                        />
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-sm-6 animation__item3">
+                    <div class="technology__box">
+                        <img
+                            src="https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg"
+                            class="img-fluid"
+                            alt="react"
+                        />
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-sm-6 animation__item5">
+                    <div class="technology__box">
+                        <img
+                            src="https://www.vectorlogo.zone/logos/git-scm/git-scm-ar21.svg"
+                            class="img-fluid"
+                            alt="git"
+                        />
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-sm-6 animation__item6">
+                    <div class="technology__box">
+                        <img
+                            src="https://www.vectorlogo.zone/logos/javascript/javascript-horizontal.svg"
+                            class="img-fluid"
+                            alt="js"
+                        />
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-sm-6 animation__item7">
+                    <div class="technology__box">
+                        <img
+                            src="https://www.vectorlogo.zone/logos/github/github-ar21.svg"
+                            class="img-fluid"
+                            alt="github"
+                        />
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-sm-6 animation__item9">
+                    <div class="technology__box">
+                        <img
+                            src="https://www.vectorlogo.zone/logos/getbootstrap/getbootstrap-ar21.svg"
+                            class="img-fluid"
+                            alt="bootstrap"
+                        />
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-sm-6 animation__item10">
+                    <div class="technology__box">
+                        <img
+                            src="https://www.vectorlogo.zone/logos/jquery/jquery-ar21.svg"
+                            class="img-fluid"
+                            alt="jquery"
+                        />
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-sm-6 animation__item11">
+                    <div class="technology__box">
+                        <img
+                            src="https://www.vectorlogo.zone/logos/visualstudio_code/visualstudio_code-icon.svg"
+                            class="img-fluid"
+                            alt="vscode"
+                        />
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-sm-6 animation__item12">
+                    <div class="technology__box">
+                        <img
+                            src="https://www.vectorlogo.zone/logos/sass-lang/sass-lang-ar21.svg"
+                            class="img-fluid"
+                            alt="sass"
+                        />
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-sm-6 animation__item14">
+                    <div class="technology__box">
+                        <img
+                            src="https://www.vectorlogo.zone/logos/npmjs/npmjs-ar21.svg"
+                            class="img-fluid"
+                            alt="npm"
+                        />
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-sm-6 animation__item13">
+                    <div class="technology__box">
+                        <img
+                            src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
+                            class="img-fluid"
+                            alt="tailwindcss"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Skills;
