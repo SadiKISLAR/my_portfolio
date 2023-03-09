84dbc7033027231878b5d1bb78db18ca23fd6fa9
SadiKISLAR <sadi22kislar@gmail.com>
2023-03-09 12:32:33 +0300

    contacts done

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,9 +1,125 @@
-import React from 'react'
-import "./contacts.css"
+import { useRef, useState } from "react";
+import "./contacts.css";
+import emailjs from "@emailjs/browser";
+import {
+    ErrorEmailToastDark,
+    ErrorEmailToastLight,
+    SendEmailToastDark,
+    SendEmailToastLight,
+} from "../../utils/customToastify";
 
 const Contacts = () => {
+    const initialState = {
+        user_name: "",
+        user_subject: "",
+        user_email: "",
+        user_message: "",
+    };
+    const formRef = useRef();
+    const [form, setForm] = useState(initialState);
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        emailjs
+            .sendForm(
+                "service_ihk4qal",
+                "template_9rdhb5g",
+                formRef.current,
+                "kvpaxHFcI6rNUTJwQ"
+            )
+            .then(
+                (result) => {
+                    {
+                        SendEmailToastLight("Email Send Successfuly");
+                    }
+                    setForm(initialState);
+                },
+                (error) => {
+                    {
+                        ErrorEmailToastLight(error.text);
+                    }
+                }
+            );
+    };
     return (
-        <div className='contacts'>Contacts</div>
+        <div className="c">
+            <div className="c-bg"></div>
+            <div className="c-wrapper">
+                <div className="c-left">
+                    <h1 className="c-title">If you want to work with me, please contact me!</h1>
+                    <div className="c-info">
+                        <div className="c-info-item">
+                            <i className="fa-solid fa-envelope c-icon fa-lg"></i>
+                            sadi22kislar@gmail.com
+                        </div>
+                    </div>
+                </div>
+                <div className="c-right">
+                    <p className="c-desc">
+                        <b>What do you want?</b> Get in touch. Always available for
+                        freelancing if the right project comes along. me.
+                    </p>
+                    <form ref={formRef} onSubmit={handleSubmit}>
+                        <input
+                            style={{
+                                backgroundColor: "blue",
+                                color: "white",
+                            }}
+                            type="text"
+                            placeholder="Name"
+                            name="user_name"
+                            value={form?.user_name || ""}
+                            onChange={handleChange}
+                            required
+                        />
+                        <input
+                            style={{
+                                backgroundColor: "blue",
+                                color: "white",
+                            }}
+                            type="text"
+                            placeholder="Subject"
+                            name="user_subject"
+                            value={form?.user_subject}
+                            onChange={handleChange}
+                            required
+                        />
+                        <input
+                            style={{
+                                backgroundColor: "blue",
+                                color: "white",
+                            }}
+                            type="email"
+                            placeholder="Email"
+                            name="user_email"
+                            value={form?.user_email}
+                            onChange={handleChange}
+                            required
+                        />
+                        <textarea
+                            style={{
+                                backgroundColor: "blue",
+                                color: "white",
+                            }}
+                            id=""
+                            rows="5"
+                            placeholder="Message"
+                            name="user_message"
+                            value={form?.user_message}
+                            onChange={handleChange}
+                            required
+                        />
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
---
+++ src/utils/customToastify.js
@@ -0,0 +1,54 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const SendEmailToastDark = (msg) => {
+  toast.info(msg, {
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+
+export const SendEmailToastLight = (msg) => {
+  toast.info(msg, {
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
+
+export const ErrorEmailToastDark = (msg) => {
+  toast.error(msg, {
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+
+export const ErrorEmailToastLight = (msg) => {
+  toast.error(msg, {
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
