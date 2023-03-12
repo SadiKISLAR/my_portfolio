5e5732cb983b1937f54c3907d743a3b687160eba
SadiKISLAR <sadi22kislar@gmail.com>
2023-03-12 17:16:26 +0300

    some changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./navbar.css"
+import { Link } from "react-scroll";
+
+import Skills from '../skills/Skills'
 
 
 const Navbar = () => {
@@ -8,8 +11,8 @@ const Navbar = () => {
             <div className='name'> -Şadi KIŞLAR- </div>
             <div className='menu'>
                 <ul>
-                    <li> <a href="#About">About</a> </li>
-                    <li> <a href="#Skills"></a> Skills</li>
+                    <li>  <a href="#About">About</a> </li>
+                    <li> <Link to={Skills} smooth={true} offset={-200} duration={300}>Skills</Link> </li>
                     <li> <a href="#Projects"></a> Projects</li>
                     <li> <a href="#Contacts"></a> Contact</li>
                 </ul>
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,134 +1,79 @@
 import React from "react";
 import "./skills.css"
+import { motion } from "framer-motion";
 
 const Skills = () => {
     return (
-        <div className="skills">
+        <div>
             <div className="skillsText">
                 <h1>SKILLS</h1>
 
                 <p>Skills and programs that I actively use on my projects</p>
             </div>
-            <div class="skillsLogo">
-                <div class="col-lg-2 col-md-4 col-sm-6  animation__item1">
-                    <div class="technology__box">
-                        <img
-                            src="https://www.vectorlogo.zone/logos/w3_html5/w3_html5-ar21.svg"
-                            class="img-fluid"
-                            alt="html"
-                        />
-                    </div>
-                </div>
+            <div className="app-skills">
 
-                <div class="col-lg-2 col-md-4 col-sm-6 animation__item2">
-                    <div class="technology__box">
-                        <img
-                            src="https://www.vectorlogo.zone/logos/w3_css/w3_css-ar21.svg"
-                            class="img-fluid"
-                            alt="css"
-                        />
-                    </div>
-                </div>
+                <motion.div className="logos "
+                    whileInView={{ scale: [0, 1] }}
+                    transition={{ duration: 1, ease: "easeInOut" }}>
+                    <motion.div className="logo" >
+                        <div className="img-div">
+                            <img src="https://www.vectorlogo.zone/logos/w3_html5/w3_html5-ar21.svg" alt="html" />
+                            <div className="logo-name">HTML</div>
+                        </div>
+                        <div className="img-div ">
+                            <img src="https://www.vectorlogo.zone/logos/w3_css/w3_css-ar21.svg" alt="css" />
+                            <div className="logo-name">CSS</div>
+                        </div>
+                        <div className="img-div ">
+                            <img src="https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg" alt="react" />
+                            <div className="logo-name">REACT</div>
+                        </div>
+                        <div className="img-div ">
+                            <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-ar21.svg" alt="git" />
+                            <div className="logo-name">GIT</div>
+                        </div>
+                        <div className="img-div ">
+                            <img src="https://www.vectorlogo.zone/logos/javascript/javascript-horizontal.svg" alt="javascript" />
+                            <div className="logo-name">JS</div>
+                        </div>
+                        <div className="img-div ">
+                            <img src="https://www.vectorlogo.zone/logos/github/github-ar21.svg" alt="github" />
+                            <div className="logo-name">GITHUB</div>
+                        </div>
+                        <div className="img-div ">
+                            <img src="https://www.vectorlogo.zone/logos/getbootstrap/getbootstrap-ar21.svg" alt="bootstrap" />
+                            <div className="logo-name">BOOTSTRAP</div>
+                        </div>
+                        <div className="img-div ">
+                            <img src="https://www.vectorlogo.zone/logos/jquery/jquery-ar21.svg" alt="jquery" />
+                            <div className="logo-name">JQUERY</div>
+                        </div>
+                        <div className="img-div ">
+                            <img src="https://www.vectorlogo.zone/logos/visualstudio_code/visualstudio_code-icon.svg" alt="vs" />
+                            <div className="logo-name">VS</div>
+                        </div>
+                        <div className="img-div ">
+                            <img src="https://www.vectorlogo.zone/logos/sass-lang/sass-lang-ar21.svg" alt="sass" />
+                            <div className="logo-name">SASS</div>
+                        </div>
+                        <div className="img-div ">
+                            <img src="https://www.vectorlogo.zone/logos/npmjs/npmjs-ar21.svg" alt="NPM" />
+                            <div className="logo-name">NPM</div>
+                        </div>
+                        <div className="img-div ">
+                            <img src="https://www.vectorlogo.zone/logos/visualstudio_code/visualstudio_code-icon.svg" alt="vs" />
+                            <div className="logo-name">VS</div>
+                        </div>
+                        <div className="img-div ">
+                            <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwind" />
+                            <div className="logo-name">TAILWIND</div>
+                        </div>
+                    </motion.div>
 
-                <div class="col-lg-2 col-md-4 col-sm-6 animation__item3">
-                    <div class="technology__box">
-                        <img
-                            src="https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg"
-                            class="img-fluid"
-                            alt="react"
-                        />
-                    </div>
-                </div>
 
-                <div class="col-lg-2 col-md-4 col-sm-6 animation__item5">
-                    <div class="technology__box">
-                        <img
-                            src="https://www.vectorlogo.zone/logos/git-scm/git-scm-ar21.svg"
-                            class="img-fluid"
-                            alt="git"
-                        />
-                    </div>
-                </div>
+                </motion.div>
 
-                <div class="col-lg-2 col-md-4 col-sm-6 animation__item6">
-                    <div class="technology__box">
-                        <img
-                            src="https://www.vectorlogo.zone/logos/javascript/javascript-horizontal.svg"
-                            class="img-fluid"
-                            alt="js"
-                        />
-                    </div>
-                </div>
 
-                <div class="col-lg-2 col-md-4 col-sm-6 animation__item7">
-                    <div class="technology__box">
-                        <img
-                            src="https://www.vectorlogo.zone/logos/github/github-ar21.svg"
-                            class="img-fluid"
-                            alt="github"
-                        />
-                    </div>
-                </div>
-
-                <div class="col-lg-2 col-md-4 col-sm-6 animation__item9">
-                    <div class="technology__box">
-                        <img
-                            src="https://www.vectorlogo.zone/logos/getbootstrap/getbootstrap-ar21.svg"
-                            class="img-fluid"
-                            alt="bootstrap"
-                        />
-                    </div>
-                </div>
-
-                <div class="col-lg-2 col-md-4 col-sm-6 animation__item10">
-                    <div class="technology__box">
-                        <img
-                            src="https://www.vectorlogo.zone/logos/jquery/jquery-ar21.svg"
-                            class="img-fluid"
-                            alt="jquery"
-                        />
-                    </div>
-                </div>
-
-                <div class="col-lg-2 col-md-4 col-sm-6 animation__item11">
-                    <div class="technology__box">
-                        <img
-                            src="https://www.vectorlogo.zone/logos/visualstudio_code/visualstudio_code-icon.svg"
-                            class="img-fluid"
-                            alt="vscode"
-                        />
-                    </div>
-                </div>
-
-                <div class="col-lg-2 col-md-4 col-sm-6 animation__item12">
-                    <div class="technology__box">
-                        <img
-                            src="https://www.vectorlogo.zone/logos/sass-lang/sass-lang-ar21.svg"
-                            class="img-fluid"
-                            alt="sass"
-                        />
-                    </div>
-                </div>
-
-                <div class="col-lg-2 col-md-4 col-sm-6 animation__item14">
-                    <div class="technology__box">
-                        <img
-                            src="https://www.vectorlogo.zone/logos/npmjs/npmjs-ar21.svg"
-                            class="img-fluid"
-                            alt="npm"
-                        />
-                    </div>
-                </div>
-
-                <div class="col-lg-2 col-md-4 col-sm-6 animation__item13">
-                    <div class="technology__box">
-                        <img
-                            src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
-                            class="img-fluid"
-                            alt="tailwindcss"
-                        />
-                    </div>
-                </div>
             </div>
         </div>
     );
