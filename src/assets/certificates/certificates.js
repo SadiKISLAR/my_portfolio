dbe368c87d2043c840fd9f09f32316d575f2de5f
SadiKISLAR <sadi22kislar@gmail.com>
2023-03-21 15:42:57 +0300

    changes in c aertificates

---
+++ src/assets/certificates/certificates.js
@@ -0,0 +1,14 @@
+import frontend from "./frontend.png";
+import react from "./react.png";
+import html_css from "./html-css.png";
+import javascript from "./javascript.png"
+
+const certificatesList = [
+    { name: "frontend", img: frontend },
+    { name: "react", img: react },
+    { name: "html_css", img: html_css },
+    { name: "javascript", img: javascript },
+    { name: "react", img: react },
+]
+
+export default certificatesList
