cc26bf5afc57e3c97a53acfc937dc26085f5d2e2
SadiKISLAR <sadi22kislar@gmail.com>
2023-03-23 11:33:37 +0300

    skills orders change

--- src/sections/index.js
+++ src/sections/index.js
@@ -2,6 +2,7 @@ import Header from "./Header/Header";
 import About from "./About/About";
 import Skills from "./Skills/Skills";
 import Projects from "./Projects/Projects";
+import Certificates from "./Certificates/Certificates"
 
 
-export { About, Header, Projects, Skills };
+export { About, Header, Projects, Certificates, Skills };
